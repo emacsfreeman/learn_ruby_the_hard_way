2f4a68479798b342de987dafc97ba290cc0b87a1
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-13 18:23:32 +0300

    add ex15.rb

---
+++ ex15.rb
@@ -0,0 +1,15 @@
+# source: https://learnrubythehardway.org/book/ex15.html
+
+filename = ARGV.first
+
+txt = open(filename)
+
+puts "Here's your file #{filename}:"
+print txt.read
+
+print "Type the filename again: "
+file_again = $stdin.gets.chomp
+
+txt_again = open(file_again)
+
+print txt_again.read
