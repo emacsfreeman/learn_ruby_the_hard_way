bc15ad78fc351f9cfbb3d3e60e87e1ba1f842b22
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-13 16:03:12 +0300

    add ex6.rb

---
+++ ex6.rb
@@ -0,0 +1,28 @@
+types_of_people = 10 # asignment of the types_of_people integer variable
+x = "There are #{types_of_people} types of people." # string interpolation
+binary = "binary" # asignment of the binary string variable
+do_not = "don't" # asignment of the do_not string variable
+
+# 1) First string inside string
+y = "Those who know #{binary} and those who #{do_not}." # string interpolation
+
+puts x # print the string x
+puts y # print the string y
+
+# 2) Second string inside string
+puts "I said: #{x}." # string interpolation
+# 3) Third string inside string
+puts "I also said: '#{y}'." # string interpolation
+
+hilarious = false # asignment of the hilarious boolean variable
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}" # string interpolation
+
+puts joke_evaluation # print string variable
+
+w = "This is the left side of..." # string asignment
+e = "a string with a right side." # string asignment
+
+puts w + e # print string concatenation
+
+# Study Drills
+# '' and "" have to be used alternately
