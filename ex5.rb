3498c54c5b7d5f404f8de77f047af97a74c25f1d
Laurent Garnier <emacsfreeman@gmail.com>
2019-06-13 12:44:04 +0300

    add ex5.rb

---
+++ ex5.rb
@@ -0,0 +1,22 @@
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 74 # inches
+height_cm = 74 * 2.54
+weight = 180 # lbs
+weight_kg = 180 * 0.45359237
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
+
+puts "Let's talk about #{name}."
+puts "He's #{height} inches tall, which gives #{height_cm} cm."
+puts "He's #{weight} pounds heavy, which gives #{weight_kg} kg."
+puts "Actually that's not too heavy."
+puts "He's got #{eyes} eyes and #{hair} hair."
+puts "His teeth are usually #{teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, and #{weight} I get #{age + height + weight}."
+
+# Study Drill
+# 2) 1 inch = 2.54 cm and 1 lb = 0.45359237 kg
